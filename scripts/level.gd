64463db66ceeb93da19b0e38f3090cdf97fd21aa
Yann Lemos <yannlemos@proton.me>
2026-08-28 19:31:26 -0300

    resources

---
+++ scripts/level.gd
@@ -0,0 +1,6 @@
+class_name Level
+extends Resource
+
+@export var boss_name: String
+@export var stage: String
+@export var preview: Texture2D
--- scripts/level_select.gd
+++ scripts/level_select.gd
@@ -2,6 +2,9 @@ extends Control
 
 @export var initial_focus: Button
 @export var boss_buttons: Array[Button] 
+
+@export var level_data: Array[Level]
+
 @onready var level_name_label: Label = %LevelNameLabel
 @onready var stage_label: Label = %StageLabel
 @onready var boss_label: Label = %BossLabel
@@ -9,19 +12,30 @@ extends Control
 @onready var tween = get_tree().create_tween()
 
 func _ready() -> void:
-	
 	initial_focus.grab_focus()
 	
+	var index := 0
 	for button in boss_buttons:
-		button.focus_entered.connect(_on_button_focus_entered.bind(button))
-		
-		
-func _on_button_focus_entered(button: Button) -> void:
-	var tween = get_tree().create_tween().bind_node(self).set_trans(Tween.TRANS_SINE)
-	tween.tween_property(%LevelNameLabel,"modulate", Color.WHITE, 0.5)
+		button.focus_entered.connect(_on_button_focus_entered.bind(button, index))
+		index += 1
+
+func _get_level(index: int) -> Level:
+	# TODO: lidar com null ref
+	#if level_data.size() <= index - 1:
+		#printerr("No level found")
+		#return null
+	return level_data[index]
+
+func _on_button_focus_entered(button: Button, level_index: int) -> void:
+	#var tween = get_tree().create_tween().bind_node(self).set_trans(Tween.TRANS_SINE)
+	#tween.tween_property(%LevelNameLabel,"modulate", Color.WHITE, 0.5)
 	
+	#print("focus entered " + button.boss_name + " " + button.stage)
 	
-	print("focus entered " + button.boss_name + " " + button.stage)
-	level_name_label.text = button.stage
-	boss_label.text = button.boss_name
-	
+	var focused_level: Level = _get_level(level_index)
+	level_name_label.text = focused_level.stage
+	boss_label.text = focused_level.boss_name
+
+#region Animations
+
+#endregion
