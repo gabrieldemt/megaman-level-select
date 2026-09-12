d26ab49b15b8813df26b060da22a34b432bb3e08
gabrieldemt <demetriogabrieel@gmail.com>
2026-09-12 16:23:07 -0300

    Add tween to button pressed

--- scripts/boss_button.gd
+++ scripts/boss_button.gd
@@ -2,3 +2,27 @@ extends Button
 
 @export var boss_name: String
 @export var stage: String
+
+var _button_pressed_tween: Tween
+
+func _ready():
+	# set pivot on bottom center
+	pivot_offset = size * Vector2(0.5, 1.0)
+	
+	button_down.connect(_on_button_down)
+	button_up.connect(_on_button_up)
+
+func _on_button_down():
+	# ensures the tween is not running
+	if _button_pressed_tween: _button_pressed_tween.kill()
+	
+	# scale down to give a pressed look
+	scale = Vector2(0.9, 0.9)
+
+func _on_button_up():
+	_button_pressed_tween = create_tween()
+	_button_pressed_tween.set_ease(Tween.EASE_OUT)
+	_button_pressed_tween.set_trans(Tween.TRANS_SPRING)
+	
+	# restores the scale smoothly in 0.15 seconds
+	_button_pressed_tween.tween_property(self, "scale", Vector2(1,1), 0.15)
